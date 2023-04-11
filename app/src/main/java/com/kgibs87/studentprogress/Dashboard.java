b69dc8c8ace1b0985dcc7bd10c40b089745d8d24
kylergib <48994502+kylergib@users.noreply.github.com>
2023-04-11 15:07:44 -0400

    Created main page that has a header and asks for your name and submit button.

---
+++ app/src/main/java/com/kgibs87/studentprogress/Dashboard.java
@@ -0,0 +1,14 @@
+package com.kgibs87.studentprogress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/com/kgibs87/studentprogress/MainActivity.java
+++ app/src/main/java/com/kgibs87/studentprogress/MainActivity.java
@@ -3,12 +3,28 @@ package com.kgibs87.studentprogress;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String debugTag = "Debug";
 
+    private EditText nameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign widgets to fields
+        nameEditText = findViewById(R.id.nameEditText);
+    }
+
+    public void submitName(View view) {
+        //get text in name edit text
+        String nameEntered = nameEditText.getText().toString();
+        if (nameEntered.equals("")) Toast.makeText(MainActivity.this, "Name cannot be blank.", Toast.LENGTH_LONG).show();
+        Log.d(debugTag, nameEntered);
     }
 }
