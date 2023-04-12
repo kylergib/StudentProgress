0079ece2abe6c9c89f111048822e384833f92bb2
kylergib <48994502+kylergib@users.noreply.github.com>
2023-04-12 16:30:44 -0400

    added assessment tests to change performance/objective tests to make sure they work properly

--- app/src/test/java/com/kgibs87/studentprogress/AssessmentUnitTest.java
+++ app/src/test/java/com/kgibs87/studentprogress/AssessmentUnitTest.java
@@ -9,25 +9,88 @@ public class AssessmentUnitTest {
 
     @Test
     public void objectiveAssessmentTest() {
+
         String assessmentTitle = "Test Assessment";
         String assessmentType = "Objective";
-        LocalDate endDate = LocalDate.of(2023,01,2);
-        Assessment testAssessment = new Assessment(assessmentType, assessmentTitle);
+        LocalDate endDate = LocalDate.of(2023,1,2);
+
+        Assessment testAssessment = defaultObjectiveAssessment();
+
+        checkMatches(testAssessment,assessmentTitle,assessmentType,endDate);
+    }
+
+    @Test
+    public void changeObjectiveAssessmentTest() {
+
+        String assessmentTitle = "New Assessment";
+        String assessmentType = "Performance";
+        LocalDate endDate = LocalDate.of(2023,1,3);
+
+        Assessment testAssessment = defaultObjectiveAssessment();
+
+        testAssessment.setAssessmentTitle(assessmentTitle);
+        testAssessment.setAssessmentType(assessmentType);
         testAssessment.setAssessmentEndDate(endDate);
 
-        boolean titleMatch = testAssessment.getAssessmentTitle().equals(assessmentTitle);
-        boolean typeMatch = testAssessment.getAssessmentType().equals(assessmentType);
-        boolean dateMatch = testAssessment.getAssessmentEndDate().equals(endDate);
-        assertTrue(titleMatch && typeMatch && dateMatch);
+        checkMatches(testAssessment,assessmentTitle,assessmentType,endDate);
+
     }
 
     @Test
     public void performanceAssessmentTest() {
+
         String assessmentTitle = "Test Assessment";
         String assessmentType = "Performance";
-        LocalDate endDate = LocalDate.of(2023,01,2);
+        LocalDate endDate = LocalDate.of(2023,1,2);
+
+
+        Assessment testAssessment = defaultPerformanceAssessment();
+
+        checkMatches(testAssessment,assessmentTitle,assessmentType,endDate);
+    }
+
+    @Test
+    public void changePerformanceAssessmentTest() {
+
+        String assessmentTitle = "New Assessment";
+        String assessmentType = "Objective";
+        LocalDate endDate = LocalDate.of(2023,1,3);
+
+        Assessment testAssessment = defaultPerformanceAssessment();
+
+        testAssessment.setAssessmentTitle(assessmentTitle);
+        testAssessment.setAssessmentType(assessmentType);
+        testAssessment.setAssessmentEndDate(endDate);
+
+        checkMatches(testAssessment,assessmentTitle,assessmentType,endDate);
+
+    }
+
+
+
+
+    public Assessment defaultPerformanceAssessment() {
+
+        String assessmentTitle = "Test Assessment";
+        String assessmentType = "Performance";
+        LocalDate endDate = LocalDate.of(2023,1,2);
         Assessment testAssessment = new Assessment(assessmentType, assessmentTitle);
         testAssessment.setAssessmentEndDate(endDate);
+        return testAssessment;
+    }
+
+    public Assessment defaultObjectiveAssessment() {
+
+        String assessmentTitle = "Test Assessment";
+        String assessmentType = "Objective";
+        LocalDate endDate = LocalDate.of(2023,1,2);
+        Assessment testAssessment = new Assessment(assessmentType, assessmentTitle);
+        testAssessment.setAssessmentEndDate(endDate);
+        return testAssessment;
+    }
+
+    public void checkMatches(Assessment testAssessment, String assessmentTitle,
+                             String assessmentType, LocalDate endDate) {
 
         boolean titleMatch = testAssessment.getAssessmentTitle().equals(assessmentTitle);
         boolean typeMatch = testAssessment.getAssessmentType().equals(assessmentType);
--- app/src/test/java/com/kgibs87/studentprogress/InstructorUnitTest.java
+++ app/src/test/java/com/kgibs87/studentprogress/InstructorUnitTest.java
@@ -8,19 +8,51 @@ import org.junit.Test;
 
 public class InstructorUnitTest {
 
+
     @Test
     public void addInstructor() {
         String name = "Test Instructor";
         String number = "555-555-5050";
         String email = "testemail@gmail.com";
 
-        Instructor newInstructor = new Instructor(name, number, email);
+        Instructor newInstructor = defaultInstructor();
 
-        boolean nameMatch = newInstructor.getInstructorName().equals(name);
-        boolean numberMatch = newInstructor.getInstructorPhoneNumber().equals(number);
-        boolean emailMatch = newInstructor.getInstructorEmail().equals(email);
+        checkInstructorProperties(newInstructor, name, number, email);
+    }
 
-        assertTrue(nameMatch && numberMatch && emailMatch);
+    @Test
+    public void changeInstructorProperties() {
+        String name = "New Instructor";
+        String number = "655-555-5050";
+        String email = "newemail@gmail.com";
+
+        Instructor newInstructor = defaultInstructor();
+
+        newInstructor.setInstructorName(name);
+        newInstructor.setInstructorPhoneNumber(number);
+        newInstructor.setInstructorEmail(email);
+
+        checkInstructorProperties(newInstructor, name, number, email);
+
 
     }
+
+    public void checkInstructorProperties(Instructor instructor, String name, String number, String email) {
+        boolean nameMatch = instructor.getInstructorName().equals(name);
+        boolean numberMatch = instructor.getInstructorPhoneNumber().equals(number);
+        boolean emailMatch = instructor.getInstructorEmail().equals(email);
+
+        assertTrue(nameMatch && numberMatch && emailMatch);
+    }
+
+    public Instructor defaultInstructor() {
+        String name = "Test Instructor";
+        String number = "555-555-5050";
+        String email = "testemail@gmail.com";
+
+
+        return new Instructor(name, number, email);
+    }
+
+
 }
