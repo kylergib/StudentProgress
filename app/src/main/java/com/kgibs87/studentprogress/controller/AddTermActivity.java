46b04758ec0b38b2f3cd82ebf83357d68304bbce
kylergib <48994502+kylergib@users.noreply.github.com>
2023-04-19 13:46:13 -0400

    replaced datePickers in AddTermActivity with DateFragments

--- app/src/main/java/com/kgibs87/studentprogress/controller/AddTermActivity.java
+++ app/src/main/java/com/kgibs87/studentprogress/controller/AddTermActivity.java
@@ -1,6 +1,8 @@
 package com.kgibs87.studentprogress.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +11,35 @@ import android.view.View;
 
 import com.kgibs87.studentprogress.R;
 
-public class AddTermActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class AddTermActivity extends AppCompatActivity implements DateFragment.OnDateSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_term);
+        //add dateFragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment dateFragmentStart = fragmentManager.findFragmentById(R.id.startDateFragmentContainer);
+        if (dateFragmentStart == null) {
+            String tag = "startDate";
+            dateFragmentStart = new DateFragment(tag);
+            fragmentManager.beginTransaction()
+                    .add(R.id.startDateFragmentContainer, dateFragmentStart,tag)
+                    .commit();
+        }
+
+        Fragment dateFragmentEnd = fragmentManager.findFragmentById(R.id.endDateFragmentContainer);
+        if (dateFragmentEnd == null) {
+            String endTag = "endDate";
+            dateFragmentEnd = new DateFragment(endTag);
+
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.endDateFragmentContainer, dateFragmentEnd,endTag)
+                    .commit();
+        }
     }
 
     public void addCourseClick(View view) {
@@ -34,5 +59,16 @@ public class AddTermActivity extends AppCompatActivity {
         //TODO: create term object and add to sqlite
 
         finish();
+    }
+    @Override
+    public void onDateSelected(Date date, String tag) {
+        // Check the tag to determine which DateFragment is invoking the method
+        if (tag.equals("startDate")) {
+//            startDate = date;
+            Log.d("AddNoteActivity", "Start date selected: " + date.toString());
+        } else if (tag.equals("endDate")) {
+//            endDate = date;
+            Log.d("AddNoteActivity", "End date selected: " + date.toString());
+        }
     }
 }
