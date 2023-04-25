59f9d3b734ff6552d0f14f9781643a0c94d1f2ef
kylergib <48994502+kylergib@users.noreply.github.com>
2023-04-25 13:14:52 -0400

    set up note activity
    18/18 tests passed

--- app/src/main/java/com/kgibs87/studentprogress/controller/AddNoteActivity.java
+++ app/src/main/java/com/kgibs87/studentprogress/controller/AddNoteActivity.java
@@ -1,68 +1,69 @@
 package com.kgibs87.studentprogress.controller;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
 import com.kgibs87.studentprogress.R;
-import com.kgibs87.studentprogress.fragment.DateFragment;
+import com.kgibs87.studentprogress.fragment.FloatingButtonFragment;
 
 import java.util.Date;
 
-public class AddNoteActivity extends AppCompatActivity implements DateFragment.OnDateSelectedListener{
-    private FragmentManager fragmentManager;
-    private Date startDate;
-    private Date endDate;
+public class AddNoteActivity extends AppCompatActivity implements FloatingButtonFragment.OnButtonClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
-        fragmentManager = getSupportFragmentManager();
-
-
-        Fragment dateFragmentStart = fragmentManager.findFragmentById(R.id.startDateFragmentContainer);
-        if (dateFragmentStart == null) {
-            String tag = "startDate";
-            dateFragmentStart = new DateFragment(tag);
-
+        Fragment addButtonFragment = fragmentManager.findFragmentById(R.id.addButtonFragmentContainer);
+        if (addButtonFragment == null) {
+            String saveTag = "saveNoteButton";
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.gravity = Gravity.END | Gravity.BOTTOM;
+            addButtonFragment = new FloatingButtonFragment(saveTag,params, R.drawable.baseline_check);
 
             fragmentManager.beginTransaction()
-                    .add(R.id.startDateFragmentContainer, dateFragmentStart,tag)
+                    .add(R.id.addButtonFragmentContainer, addButtonFragment,saveTag)
                     .commit();
         }
 
-        Fragment dateFragmentEnd = fragmentManager.findFragmentById(R.id.endDateFragmentContainer);
-        if (dateFragmentEnd == null) {
-            String endTag = "endDate";
-            dateFragmentEnd = new DateFragment(endTag);
-
-
+        Fragment backButtonFragment = fragmentManager.findFragmentById(R.id.backButtonFragmentContainer);
+        if (backButtonFragment == null) {
+            String cancelTag = "cancelNoteButton";
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.gravity = Gravity.START | Gravity.BOTTOM;
+            backButtonFragment = new FloatingButtonFragment(cancelTag,params,R.drawable.baseline_close);
             fragmentManager.beginTransaction()
-                    .add(R.id.endDateFragmentContainer, dateFragmentEnd,endTag)
+                    .add(R.id.addButtonFragmentContainer, backButtonFragment,cancelTag)
                     .commit();
         }
-
-    }
-    public void testClick(View view) {
 
     }
 
     @Override
-    public void onDateSelected(Date date, String tag) {
-        // Check the tag to determine which DateFragment is invoking the method
-        if (tag.equals("startDate")) {
-            startDate = date;
-            Log.d("AddNoteActivity", "Start date selected: " + date.toString());
-        } else if (tag.equals("endDate")) {
-            endDate = date;
-            Log.d("AddNoteActivity", "End date selected: " + date.toString());
+    public void onButtonClick(View view, String tag) {
+        if (tag.equals("cancelNoteButton")) {
+            Log.d("Back tag", tag);
+            finish();
+        } else if (tag.equals("saveNoteButton")) {
+            Log.d("Add tag", tag);
+            //TODO: create term object and add to sqlite
+            finish();
         }
     }
 }
